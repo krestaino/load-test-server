1dc40b2d88a26a3d70d72e08f11531b331717ab2
Kevin Restaino <kevinrestaino@gmail.com>
2019-12-08 19:11:18 -0600

    async route

--- src/index.js
+++ src/index.js
@@ -6,13 +6,16 @@ require('dotenv').config()
 const { SERVER_NAME, SERVER_PORT } = process.env;
 
 const api = express();
+const response = { server: SERVER_NAME, status: "success" }
+
 api.use(cors());
 
-api.get('/test', async ({ res }) => {
-  setTimeout(async () => res.send({
-    server: SERVER_NAME,
-    status: "success"
-  }), 5000);
+api.get('/', ({ res }) => {
+  res.send(response)
+});
+
+api.get('/async', async ({ res }) => {
+  setTimeout(async () => res.send(response), 5000);
 });
 
 api.listen(SERVER_PORT, () => console.info('Server: Accepting connections at http://localhost:' + SERVER_PORT));
