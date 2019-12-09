a798f4644eb1af6cc0e17a8350bf076e53b3ba38
Kevin Restaino <kevinrestaino@gmail.com>
2019-12-08 19:03:44 -0600

    env

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import cors from 'cors';
 import express from 'express';
 
-const SERVER_PORT = 1337;
+require('dotenv').config()
+
+const { SERVER_NAME, SERVER_PORT } = process.env;
 
 const api = express();
 api.use(cors());
 
 api.get('/', async ({ res }) => {
-  setTimeout(async () => res.send({ status: "success" }), 5000);
+  setTimeout(async () => res.send({
+    server: SERVER_NAME,
+    status: "success"
+  }), 5000);
 });
 
 api.listen(SERVER_PORT, () => console.info('Server: Accepting connections at http://localhost:' + SERVER_PORT));
